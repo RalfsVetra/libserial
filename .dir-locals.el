b687370ab9c1e3363d70c813dc46faab4d15c9c7
RalfsVetra <106433391+RalfsVetra@users.noreply.github.com>
2026-06-26 21:49:50 +0300

    style: fix formatting rules in editor configs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
-((c++-mode . ((c-file-style . "stroustrup"))))
-
+((c++-mode . ((c-file-style . "stroustrup")
+              (indent-tabs-mode . nil))))
--- src/serial.cc
+++ src/serial.cc
@@ -21,7 +21,7 @@ serial::serial::serial(port_opt opt) :
     fd{-1}
 {
     if (opt.device.empty()) [[unlikely]]
-	throw std::invalid_argument("Device path cannot be empty.");
+        throw std::invalid_argument("Device path cannot be empty.");
 }
 
 serial::serial::~serial() {
@@ -31,8 +31,8 @@ serial::serial::~serial() {
 void serial::serial::close()
 {
     if (fd >= 0) {
-	::close(fd);
-	fd = -1;
+        ::close(fd);
+        fd = -1;
     }
 }
 
@@ -41,8 +41,8 @@ void serial::serial::open()
     fd = ::open(opt.device.c_str(), O_RDWR | O_NOCTTY);
 
     if (fd < 0) [[unlikely]] {
-	throw std::system_error(errno, std::generic_category(),
-				"Failed to open " + opt.device);
+        throw std::system_error(errno, std::generic_category(),
+                                "Failed to open " + opt.device);
     }
 
     conf_port();
@@ -52,19 +52,19 @@ void set_byte_size(termios& tty, const serial::byte_size size)
 {
     switch (size) {
     case serial::byte_size::five:
-	tty.c_cflag |= CS5;
-	break;
+        tty.c_cflag |= CS5;
+        break;
     case serial::byte_size::six:
-	tty.c_cflag |= CS6;
-	break;
+        tty.c_cflag |= CS6;
+        break;
     case serial::byte_size::seven:
-	tty.c_cflag |= CS7;
-	break;
+        tty.c_cflag |= CS7;
+        break;
     case serial::byte_size::eight:
-	tty.c_cflag |= CS8;
-	break;
+        tty.c_cflag |= CS8;
+        break;
     default:
-	throw std::range_error("Unrecognized byte size");
+        throw std::range_error("Unrecognized byte size");
     }
 }
 
@@ -72,31 +72,31 @@ void set_parity(termios& tty, const serial::parity parity)
 {
     switch (parity) {
     case serial::parity::none:
-	break;
+        break;
     case serial::parity::odd:
-	tty.c_cflag |= (PARENB | PARODD);
-	break;
+        tty.c_cflag |= (PARENB | PARODD);
+        break;
     case serial::parity::even:
-	tty.c_cflag |= PARENB;
-	tty.c_cflag &= ~PARODD;
-	break;
+        tty.c_cflag |= PARENB;
+        tty.c_cflag &= ~PARODD;
+        break;
     case serial::parity::mark:
 #ifdef CMSPAR
-	tty.c_cflag |= (PARENB | CMSPAR | PARODD);
-	break;
+        tty.c_cflag |= (PARENB | CMSPAR | PARODD);
+        break;
 #else
-	throw std::invalid_argument("Mark parity is not supported");
+        throw std::invalid_argument("Mark parity is not supported");
 #endif
     case serial::parity::space:
 #ifdef CMSPAR
-	tty.c_cflag |= (PARENB | CMSPAR);
-	tty.c_cflag &= ~PARODD;
-	break;
+        tty.c_cflag |= (PARENB | CMSPAR);
+        tty.c_cflag &= ~PARODD;
+        break;
 #else
-	throw std::invalid_argument("Space parity is not supported");
+        throw std::invalid_argument("Space parity is not supported");
 #endif
     default:
-	throw std::range_error("Unrecognized parity");
+        throw std::range_error("Unrecognized parity");
     }
 }
 
@@ -104,13 +104,13 @@ void set_stop_bit(termios& tty, const serial::stop_bit stop)
 {
     switch (stop) {
     case serial::stop_bit::one:
-	tty.c_cflag &= ~CSTOPB;
-	break;
+        tty.c_cflag &= ~CSTOPB;
+        break;
     case serial::stop_bit::two:
-	tty.c_cflag |= CSTOPB;
-	break;
+        tty.c_cflag |= CSTOPB;
+        break;
     default:
-	throw std::range_error("Unrecognized stop bit");
+        throw std::range_error("Unrecognized stop bit");
     }
 }
 
@@ -118,34 +118,34 @@ void set_flow_control(termios& tty, const serial::flow_control flow)
 {
     switch (flow) {
     case serial::flow_control::none:
-	tty.c_cflag &= ~CRTSCTS;
-	tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-	break;
+        tty.c_cflag &= ~CRTSCTS;
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+        break;
     case serial::flow_control::hardware:
-	tty.c_cflag |= CRTSCTS;
-	break;
+        tty.c_cflag |= CRTSCTS;
+        break;
     case serial::flow_control::software:
-	tty.c_iflag |= (IXON | IXOFF | IXANY);
-	break;
+        tty.c_iflag |= (IXON | IXOFF | IXANY);
+        break;
     default:
-	throw std::range_error("Unrecognized flow control");
+        throw std::range_error("Unrecognized flow control");
     }
 }
 
 void set_timeout(termios& tty, const std::int16_t timeout_ms)
 {
     if (timeout_ms > 25'500)
-	throw std::out_of_range("Timeout cannot exceed 25500 milliseconds");
+        throw std::out_of_range("Timeout cannot exceed 25500 milliseconds");
 
     if (timeout_ms > 0) {
-	tty.c_cc[VMIN] = 0;
-	tty.c_cc[VTIME] = static_cast<cc_t>((timeout_ms + 99) / 100);
+        tty.c_cc[VMIN] = 0;
+        tty.c_cc[VTIME] = static_cast<cc_t>((timeout_ms + 99) / 100);
     } else if (timeout_ms < 0) {
-	tty.c_cc[VMIN] = 1;
-	tty.c_cc[VTIME] = 0;
+        tty.c_cc[VMIN] = 1;
+        tty.c_cc[VTIME] = 0;
     } else {
-	tty.c_cc[VMIN] = 0;
-	tty.c_cc[VTIME] = 0;
+        tty.c_cc[VMIN] = 0;
+        tty.c_cc[VTIME] = 0;
     }
 }
 
@@ -154,16 +154,16 @@ void serial::serial::conf_port()
     struct termios tty;
 
     if (::tcgetattr(fd, &tty) < 0) {
-	throw std::system_error(errno, std::generic_category(),
-				"Failed to get port stats");
+        throw std::system_error(errno, std::generic_category(),
+                                "Failed to get port stats");
     }
 
     ::cfmakeraw(&tty);
 
     const speed_t mapped_bit_rate = static_cast<speed_t>(opt.rate);
     if (::cfsetspeed(&tty, mapped_bit_rate) < 0) [[unlikely]] {
-	throw std::system_error(errno, std::generic_category(),
-				"Failed to set speed");
+        throw std::system_error(errno, std::generic_category(),
+                                "Failed to set speed");
     }
 
     set_byte_size(tty, opt.size);
@@ -173,7 +173,7 @@ void serial::serial::conf_port()
     set_timeout(tty, opt.timeout);
 
     if (::tcsetattr(fd, TCSANOW, &tty) < 0) {
-	throw std::system_error(errno, std::generic_category(),
-				"Failed to set attributes");
+        throw std::system_error(errno, std::generic_category(),
+                                "Failed to set attributes");
     }
 }
