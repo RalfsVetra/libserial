ab4f739c6c911b6bd3213b61845972d19d256ed1
RalfsVetra <106433391+RalfsVetra@users.noreply.github.com>
2026-06-27 23:10:29 +0300

    feat: add demo project

---
+++ demo/main.cc
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2026 Ralfs K. Vētra
+//
+// SPDX-License-Identifier: MIT
+
+// #include <vector>
+#include <cstdint>
+#include <array>
+
+import serial;
+
+using namespace serial;
+
+int main()
+{
+    Serial port({
+        .port{"/dev/ttyACM0"},
+        .rate{bit_rate::b115200},
+        .parity{parity_mode::odd}
+    });
+
+    // const std::vector<std::uint8_t> buf(11, 0);
+    // const std::uint8_t buf[] = {1, 2, 3};
+    const std::array<std::uint8_t, 3> buf{1, 2, 3};
+    auto bytes_written = port.write(buf);
+
+    // std::vector<std::uint8_t> buf(11, 0);
+    // std::uint8_t buf[64] = {};
+    // std::array<std::uint8_t, 11> buf{};
+    // auto bytes_read = port.read(buf);
+}
