fc61c0005d01c715ad417470446aae28932075bd
RalfsVetra <106433391+RalfsVetra@users.noreply.github.com>
2026-07-01 21:29:00 +0300

    feat: add unit tests

---
+++ test/UnitTests.cc
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2026 Ralfs K. Vētra
+//
+// SPDX-License-Identifier: MIT
+
+#include <array>
+#include <cstdint>
+#include <gtest/gtest.h>
+
+import serial;
+
+namespace {
+class SerialTest : public testing::Test {
+protected:
+  serial::Serial Port{
+      {.Port{"/dev/ttyACM0"}, .Parity{serial::ParityMode::Odd}}};
+};
+} // namespace
+
+TEST_F(SerialTest, GetTimeout) { EXPECT_EQ(-1, Port.getTimeout()); }
+
+TEST_F(SerialTest, SetTimeout) {
+  Port.setTimeout(500);
+  EXPECT_EQ(500, Port.getTimeout());
+}
+
+// TEST_F(SerialTest, WritePort) {
+//   std::array<std::uint8_t, 11> Buf{};
+//   EXPECT_EQ(11, Port.write(Buf));
+// }
+
+// TEST_F(SerialTest, ReadPort) {
+//   std::array<std::uint8_t, 11> Buf{};
+//   EXPECT_EQ(11, Port.read(Buf));
+// }
